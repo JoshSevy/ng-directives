6c130c039ac92ed8bb6df24ca767350443b05074
JoshSevy <joshuasevy@outlook.com>
2020-11-29 16:05:08 -0700

    Practice with ngswitch statements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    BasicHighlightDirective, BetterHighlightDirective
+    BasicHighlightDirective, BetterHighlightDirective, UnlessDirective
   ],
   imports: [
     BrowserModule,
